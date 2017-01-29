b14423cdc9a700346ddbc07a2bc65a65bbdf88ae
Anand Bhoraskar <anand@Anands-MacBook-Pro.local>
2017-01-30 01:58:34 +0530

    Removed unnecessary line of code

--- HW1_CameraGeometry/HW1_CameraGeometry/Q5/code/myMainScript.m
+++ HW1_CameraGeometry/HW1_CameraGeometry/Q5/code/myMainScript.m
@@ -21,11 +21,9 @@ err = err*0.05;
 f3D_err = f3D(1:3, :);
 f2D_err = f2D(1:2, :);
 f3D_err = f3D_err + err*randn(size(f3D_err));
-f3D_err = [f3D_err; ones(1,size(f3D,2))];
 f2D_err = f2D_err + err*randn(size(f2D_err));
-f2D_err = [f2D_err; ones(1,size(f2D,2))];
 
-M_err = getCalib(f3D_err(1:3, :), f2D_err(1:2, :));
+M_err = getCalib(f3D_err, f2D_err);
 disp('Validating M_err');
 findError(f3D, f2D, M_err);
 
