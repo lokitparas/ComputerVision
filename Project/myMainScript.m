a9b89893f1b4181dad04067f4bd10913acd85a49
Anand Bhoraskar <anand@Anands-MacBook-Pro.local>
2017-03-21 17:43:41 +0530

    For writing and input o video

---
+++ Project/myMainScript.m
@@ -0,0 +1,14 @@
+addpath('./MMread');
+
+path = '/Users/anand/Desktop/sem8/vision/assgn/Project';
+fileName = 'vid.mp4';
+fileFullName = strcat(path, '/', fileName);
+
+[video, audio_orig] = mmread(fileFullName);
+
+vid = zeros(360, 640, 1179);
+for i = 1:video.nrFramesTotal
+    size(rgb2gray(video.frames(i).cdata));
+    vid(:,:,i) = rgb2gray(video.frames(i).cdata);
+end
+writevideo('op.avi', vid, video.rate);
