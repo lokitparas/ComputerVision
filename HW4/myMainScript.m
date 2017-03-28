8ae7ab3d886006ae05c90c9684c71268d3a11f38
Anand Bhoraskar <anandbhoraskar@gmail.com>
2017-03-29 02:32:08 +0530

    Parts 1-4 done - explanation for 4th

--- HW4/myMainScript.m
+++ HW4/myMainScript.m
@@ -1,8 +1,10 @@
 % Read X[n,d], Y[n,1] 
 
 A = [];
-load 'dataset2.mat' A;
-
+load 'dataset4.mat' A;
+A = A(randperm(2000), :);
+% B = A([1001:2000 1:1000], :);
+% A = B;
 test_data = A(1001:end,1:2);
 test_labels = A(1001:end,3);
 A = A(1:1000, :);
@@ -11,7 +13,7 @@ Y = A(:,3);
 
 
 % Training
-T = 40;
+T = 1000;
 row = size(X,1);
 col = size(X,2);
 w = ones(row,1)/ row;
@@ -19,7 +21,7 @@ H = zeros(T,4);
 train_err = zeros(T,1);
 test_err = zeros(T,1);
 for t = 1:T
-    disp('t-> ');
+    disp(t);
     [ i, theta, p, alpha, w ] = adaboost(X, Y, w);
 %     temp = [i, theta, p, alpha];
     H(t, :) = [i, theta, p, alpha];
@@ -32,7 +34,8 @@ end
 
 [pred, ~] = computeError(H(1:t, :), test_data, test_labels);
 plot(test_err);
-figure();scatter(test_data(:,1), test_data(:,2), 30, pred+2, 'x');
+figure();scatter(test_data(:,1), test_data(:,2), 30, pred, 'x');
+figure();scatter(test_data(:,1), test_data(:,2), 30, test_labels, 'x');
 
 % Testing
 
