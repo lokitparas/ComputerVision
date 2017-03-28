fb0c8f7070bcc0a57f23334c2c76715d66b42390
Anand Bhoraskar <anandbhoraskar@gmail.com>
2017-03-29 02:04:13 +0530

    added plots

---
+++ HW4/computeError.m
@@ -0,0 +1,17 @@
+function [ pred, error ] = computeError( H, X, Y )
+%COMPUTEERROR Summary of this function goes here
+%   Detailed explanation goes here
+pred = zeros(size(Y));
+for it = 1:size(H, 1)
+    op = H(it, :);
+    i = op(1,1);
+    theta = op(1,2);
+    p = op(1,3);
+    alpha = op(1,4);
+    pred = pred + ((X(:,i)-theta >= 0)*2-1)*p*alpha;    
+end
+pred = (pred>=0)*2-1;
+
+error = sum(Y~=pred)/size(Y,1);
+end
+
--- HW4/myMainScript.m
+++ HW4/myMainScript.m
@@ -1,9 +1,14 @@
 % Read X[n,d], Y[n,1] 
 
 A = [];
-load 'dataset1.mat' A;
+load 'dataset2.mat' A;
+
+test_data = A(1001:end,1:2);
+test_labels = A(1001:end,3);
+A = A(1:1000, :);
 X = A(:,1:2); 
 Y = A(:,3);
+
 
 % Training
 T = 40;
@@ -11,15 +16,23 @@ row = size(X,1);
 col = size(X,2);
 w = ones(row,1)/ row;
 H = zeros(T,4);
+train_err = zeros(T,1);
+test_err = zeros(T,1);
 for t = 1:T
     disp('t-> ');
     [ i, theta, p, alpha, w ] = adaboost(X, Y, w);
 %     temp = [i, theta, p, alpha];
     H(t, :) = [i, theta, p, alpha];
-    disp(H(t,:));
+%     disp(H(t,:));
     
+%     For printing the training error
+    [~, train_err(t,1)] = computeError(H(1:t, :), X, Y);
+    [~, test_err(t,1)] = computeError(H(1:t, :), test_data, test_labels);
 end
 
+[pred, ~] = computeError(H(1:t, :), test_data, test_labels);
+plot(test_err);
+figure();scatter(test_data(:,1), test_data(:,2), 30, pred+2, 'x');
 
 % Testing
 
