ef36536fb323f6963c69f939c6822a7336c90e48
Anand Bhoraskar <anandbhoraskar@gmail.com>
2017-04-16 02:37:43 +0530

    reference project added

--- Project/myMainScript.m
+++ Project/myMainScript.m
@@ -6,9 +6,29 @@ fileFullName = strcat(path, '/', fileName);
 
 [video, audio_orig] = mmread(fileFullName);
 
-vid = uint8(zeros(360, 640, 1179));
+vid = uint8(zeros(video.height, video.width, video.nrFramesTotal));
 for i = 1:video.nrFramesTotal
     size(rgb2gray(video.frames(i).cdata));
     vid(:,:,i) = rgb2gray(video.frames(i).cdata);
 end
+
+imshow(vid(:,:,1));
+hold on;
+% First click on top left corner and 
+% [x, y] = getpts();
+hold off;
+drawEllipse(x(1), y(1), x(2), y(2));
+
+for i = 2:video.nrFramesTotal
+    
+    [y0_vals, y0_locs] = pointsFromEllipse(x(1), y(1), x(2), y(2));
+    
+    imshow(vid(:,:,i));
+    hold on;
+    drawEllipse(x(1), y(1), x(2), y(2));
+    hold off;
+    pause(0.1)
+end
+
+
 writevideo('op.avi', vid, video.rate);
