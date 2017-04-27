5bf66c2c6f5f396a1b734db96205f2482dab14ed
Anand Bhoraskar <anandbhoraskar@gmail.com>
2017-04-27 10:44:10 +0530

    scale added but not working

--- Project/getWeights.m
+++ Project/getWeights.m
@@ -1,6 +1,17 @@
-function w =  getWeights(y0_bins, q, p, num_bins)
+function w =  getWeights(y0_bins, q, p, num_bins, points, center, ax)
     w = zeros(size(y0_bins));
     for i = 1:num_bins
         w(y0_bins == i) = sqrt(q(i)/(p(i)+eps));
     end
+
+
+	scaled_dist = (bsxfun(@minus,points,center).^2);
+	scaled_dist = bsxfun(@rdivide, scaled_dist, (ax.^2));
+	scaled_dist = sum(scaled_dist, 2);
+	w.*kernel_derivative(scaled_dist);
+end
+
+function val = kernel_derivative(x)
+val = zeros(size(x));
+val(x<1) = 3/(2*pi);
 end
--- Project/meanShift.m
+++ Project/meanShift.m
@@ -2,32 +2,33 @@ function [new_ax, new_center] = meanShift(image, ax_prev, center, q, num_bins)
 num_itt = 0;
 max_bhatta = 0;
 
-for ax_ratio = 0.9:0.01:1.1
+for ax_ratio = 1
     ax = ax_ratio*ax_prev;
     while (num_itt < 2000)
         [points, bins, p0] = getProfile(image, ax, center, num_bins);
         bhatta0 = sqrt(dot(p0, q));
 
-        w = getWeights(bins, q, p0, num_bins);
+        w = getWeights(bins, q, p0, num_bins, points, center, ax);
         sum_w = sum(w);
         center1 = sum([w.*points(:,1) w.*points(:,2)])/sum_w;
         [~, ~, p1] = getProfile(image, ax, center1, num_bins);
         bhatta1 = sqrt(dot(p1, q));
         
-        if (bhatta1 < bhatta0)
-            center1 = center;
-            bhatta1 = bhatta0;
+        while (bhatta1 < bhatta0) & (dot(center1-center, center1-center) > 0.01)
+            center1 = (center+center1)/2;
+            [~, ~, p1] = getProfile(image, ax, center1, num_bins);
+            bhatta1 = sqrt(dot(p1, q));
+        end
+
+        if dot(center1-center, center1-center) < 0.01
+            center1 = round(center1);
+            % bhatta1 = bhatta1;
             break;
         else
-            if dot(center1-center, center1-center) < 0.5
-                center1 = round(center1);
-                % bhatta1 = bhatta1;
-                break;
-            else
-                center = center1;
-                bhatta0 = bhatta1;
-            end
+            center = center1;
+            bhatta0 = bhatta1;
         end
+
         num_itt = num_itt+1;
     end
     if bhatta1 > max_bhatta
@@ -38,5 +39,7 @@ for ax_ratio = 0.9:0.01:1.1
         max_bhatta = bhatta1;
     end
 end
+
+disp(max_bhatta);
 
 end
